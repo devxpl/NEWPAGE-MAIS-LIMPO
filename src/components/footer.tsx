0bd9d5ead40d7b4f3f92a03e89132bee630e82fd
devxpl <dev-xpl@hotmail.com>
2023-04-14 00:06:03 -0300

    commit

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,37 +1,25 @@
 export const Footer = () => {
   return (
-    <footer className="bg-gray-800 py-12">
-      <div className="container mx-auto">
-        <div className="flex flex-wrap">
-          <div className="w-full md:w-1/2 lg:w-1/3 mb-8">
-            <h3 className="text-white font-bold mb-4">Contato</h3>
-            <ul className="text-gray-400">
-              <li>Email: contato@exemplo.com</li>
-              <li>Telefone: (00) 0000-0000</li>
-              <li>Endereço: Rua Exemplo, 123 - Bairro Exemplo</li>
-            </ul>
-          </div>
-          <div className="w-full md:w-1/2 lg:w-1/3 mb-8">
-            <h3 className="text-white font-bold mb-4">Siga-nos</h3>
-            <ul className="text-gray-400">
-              <li>
-                <a href="#">
-                  <i className="fab fa-facebook"></i> Facebook
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <i className="fab fa-twitter"></i> Twitter
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <i className="fab fa-instagram"></i> Instagram
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+    <footer className="py-5">
+      <div className="flex flex-wrap justify-center">
+        <h3 className="text-blue-400 font-bold mb-4">Contatos</h3>
+        <ul className="text-gray-400">
+          <li className="inline-block pl-2">
+            <a href="#">
+              <i className="fab fa-facebook"></i> Facebook
+            </a>
+          </li>
+          <li className="inline-block pl-2">
+            <a href="#">
+              <i className="fab fa-twitter "></i> WhatsApp
+            </a>
+          </li>
+          <li className="inline-block pl-2">
+            <a href="#">
+              <i className="fab fa-instagram"></i> Instagram
+            </a>
+          </li>
+        </ul>
       </div>
     </footer>
   );
